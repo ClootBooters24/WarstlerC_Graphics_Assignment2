f85094749f70381e83999d5bc076164d79055e32
Clayton Warstler <claywarstler@gmail.com>
2024-02-29 21:22:53 -0600

    Finished project, need to finish doc

--- UFO.cpp
+++ UFO.cpp
@@ -5,6 +5,7 @@
 // Date:    Spring 2012
 //---------------------------------------
 #include <math.h>
+#include <chrono>
 #include <stdio.h>
 #include <stdlib.h>
 #ifdef MAC
@@ -12,6 +13,8 @@
 #else
 #include <GL/glut.h>
 #endif
+using namespace std::chrono;
+
 
 // Global constants
 #define MIN_X_VIEW -50
@@ -24,16 +27,62 @@
 #define MAX_X_SCREEN 500
 #define MIN_Y_SCREEN 0
 #define MAX_Y_SCREEN 500
-#define LINE_COUNT 1000
-#define MAX_COUNT 100
+#define LINE_COUNT 100000
+#define MAX_COUNT 10000
 
 // Global variables 
 float Px[MAX_COUNT], Py[MAX_COUNT];
-int xangle = 10;
-int yangle = 15;
-int mode = 1;
 int count = 0;
-float point[LINE_COUNT][4];
+float coords[MAX_COUNT][MAX_COUNT];
+int node = 0;
+bool drawn = false;
+bool ufoStart = false;
+
+void ufo() {
+   static int currentNode = 0;
+   static float progress = 0.0;
+
+   if (currentNode >= count) {
+      currentNode = 0;
+      progress = 0.0;
+      return;
+   }
+
+   progress += 0.01;
+   if (progress > 1.0) {
+      progress = 0.0;
+      currentNode++;
+   }
+
+   //Get and print current coordinates
+   float currentX = coords[currentNode][0] + (coords[currentNode + 1][0] - coords[currentNode][0]) * progress;
+   float currentY = coords[currentNode][1] + (coords[currentNode + 1][1] - coords[currentNode][1]) * progress;
+   printf("Current position: (%f, %f)\n", currentX, currentY);
+
+   //Get direction and angle of the line
+   float dx = coords[currentNode + 1][0] - coords[currentNode][0];
+   float dy = coords[currentNode + 1][1] - coords[currentNode][1];
+   float angle = atan2(dy, dx);
+
+   float size = 10.0;
+   float halfSize = size / 2.0;
+
+   glPushMatrix();
+   glLoadIdentity();
+   glTranslatef(currentX, currentY, 0.0);
+   glRotatef(angle * 180 / M_PI, 0, 0, 1); // Rotate to align with the line segment
+
+   //Draw triangle
+   glBegin(GL_POLYGON);
+   glColor3f(1.0, 0.0, 0.0);
+   glVertex2f(0.0, halfSize); 
+   glVertex2f(-size, -halfSize); 
+   glVertex2f(-size, halfSize); 
+   glEnd();
+
+   glPopMatrix();
+   glutPostRedisplay();
+}
 
 //---------------------------------------
 // Init function for OpenGL
@@ -50,66 +99,16 @@ void init()
 }
 
 //---------------------------------------
-// Function to draw a UFO
-//---------------------------------------
-void UFO(float x, float y, float z)
-{
-   // Draw UFO body
-   glColor3f(0.0, 0.0, 1.0);
-   glPushMatrix();
-   glTranslatef(x, y, z);
-   glutSolidSphere(5, 20, 20);
-   glPopMatrix();
-
-   // Draw UFO top
-   glColor3f(1.0, 0.0, 0.0);
-   glPushMatrix();
-   glTranslatef(x, y, z);
-   glRotatef(90, 1, 0, 0);
-   glutSolidCone(5, 10, 20, 20);
-   glPopMatrix();
-
-   mode = 2;
-}
-
-//---------------------------------------
-// Function to draw a zero thickness wall
-//---------------------------------------
-void wall(float x1, float y1, float x2, float y2)
-{
-   float z1 = 0;
-   float z2 = 10;
-
-   // Draw wall polygon
-   glColor3f(1.0, 0.0, 0.0);
-   glBegin(GL_POLYGON);
-   glVertex3f(x1, y1, z1);
-   glVertex3f(x2, y2, z1);
-   glVertex3f(x2, y2, z2);
-   glVertex3f(x1, y1, z2);
-   glEnd();
-
-   // Draw wall outline
-   glColor3f(1.0, 1.0, 1.0);
-   glBegin(GL_LINE_LOOP);
-   glVertex3f(x1, y1, z1);
-   glVertex3f(x2, y2, z1);
-   glVertex3f(x2, y2, z2);
-   glVertex3f(x1, y1, z2);
-   glEnd();
-}
-
-//---------------------------------------
 // Keyboard callback for OpenGL
 //---------------------------------------
 void keyboard(unsigned char key, int x, int y)
 {
    // Update angles
-   if (key == 'u')
-      UFO(x,y,0);
-   
-   // Redraw objects
-   glutPostRedisplay();
+   if(key == 'u') {
+      ufoStart = true;
+      ufo();
+      glutPostRedisplay();
+   }
 }
 
 //---------------------------------------
@@ -123,31 +122,23 @@ void mouse(int button, int state, int x, int y)
    float y_scale = (MIN_Y_VIEW - MAX_Y_VIEW) / 
       (float)(MAX_Y_SCREEN - MIN_Y_SCREEN);
 
+   // Initialize Px and Py arrays with the first click coordinates
+   if (count == 0) {
+      Px[0] = MIN_X_VIEW + (x - MIN_X_SCREEN) * x_scale;
+      Py[0] = MAX_Y_VIEW + (y - MIN_Y_SCREEN) * y_scale;
+      coords[0][0] = Px[0], coords[0][1] = Py[0];
+   }
+
    // Handle mouse down
    if (state == GLUT_DOWN)
    {
+      // Increment count and store coordinates
       if (count < MAX_COUNT) count++; 
       Px[count] = MIN_X_VIEW + (x - MIN_X_SCREEN) * x_scale;
       Py[count] = MAX_Y_VIEW + (y - MIN_Y_SCREEN) * y_scale;
+      coords[count][0] = Px[count], coords[count][1] = Py[count];
       glutPostRedisplay();
    }
-}
-
-//---------------------------------------
-// Motion callback for OpenGL
-//---------------------------------------
-void motion(int x, int y)
-{
-   // Calculate scale factors
-   float x_scale = (MAX_X_VIEW - MIN_X_VIEW) / 
-      (float)(MAX_X_SCREEN - MIN_X_SCREEN);
-   float y_scale = (MIN_Y_VIEW - MAX_Y_VIEW) / 
-      (float)(MAX_Y_SCREEN - MIN_Y_SCREEN);
-
-   // Handle mouse motion
-   point[count][2] = MIN_X_VIEW + (x - MIN_X_SCREEN) * x_scale;
-   point[count][3] = MAX_Y_VIEW + (y - MIN_Y_SCREEN) * y_scale;
-   glutPostRedisplay();
 }
 
 //---------------------------------------
@@ -159,14 +150,6 @@ void display()
    glMatrixMode(GL_MODELVIEW);
    glLoadIdentity();
 
-   // Draw control points
-   glColor3f(1.0, 1.0, 1.0);
-   glPointSize(3);
-   glBegin(GL_POINTS);
-   for (int i=0; i<=count; i++)
-      glVertex2f(Px[i], Py[i]);
-   glEnd();
-
    // Connect control points
    glLineWidth(2);
    glBegin(GL_LINE_STRIP);
@@ -174,13 +157,11 @@ void display()
       glVertex2f(Px[i], Py[i]);
    glEnd();
 
-   if(mode == 2) {
-      for (int i=0; i<=count; i++) {
-         UFO(point[i][2], point[i][3], 0);
-      }
+   if (ufoStart) {
+      ufo();
    }
-
-   glFlush();
+   
+   glutSwapBuffers(); // Use double buffering for smooth rendering
 }
 
 //---------------------------------------
@@ -196,7 +177,6 @@ int main(int argc, char *argv[])
    glutDisplayFunc(display);
    glutKeyboardFunc(keyboard);
    glutMouseFunc(mouse);
-   glutMotionFunc(motion);
    init();
    printf("Keyboard commands:\n");
    printf("   'u' - fly the UFO across the lines\n");
